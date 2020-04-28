b2d8205538464b1a5879dcff60472b37b98e214b
lsnbr <luis@sovt.de>
2020-04-28 15:06:16 +0200

    profiling decorator

---
+++ speed.py
@@ -0,0 +1,23 @@
+from cProfile import Profile
+import pstats
+
+
+
+def profile(sort_args=['cumulative'], print_args=[16]):
+
+    profiler = Profile()
+
+    def decorator(fn):
+        def inner(*args, **kwargs):
+
+            result = None
+
+            try:
+                result = profiler.runcall(fn, *args, **kwargs)
+            finally:
+                stats = pstats.Stats(profiler)
+                stats.strip_dirs().sort_stats(*sort_args).print_stats(*print_args)
+
+            return result
+        return inner
+    return decorator
