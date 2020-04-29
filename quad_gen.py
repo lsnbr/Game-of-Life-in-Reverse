d6b61b8c60b44ffd34f19dd9247fa64484c7737a
lsnbr <luis@sovt.de>
2020-04-29 16:02:43 +0200

    skeleton quad_gen

--- quad_gen.py
+++ quad_gen.py
@@ -2,6 +2,20 @@ import numpy as np
 
 from gol_tools import *
 from square_cell import SquareCell, merge_quad
+from square_tree import SquareTree
 
 
 
+def quad_gen(goal):
+
+    height, width = goal.shape
+    tree = SquareTree(goal.shape)
+    init_cells = np.ndarray(goal.shape, dtype=Squarecell)
+    # dict of all kid of 3x3 predecessors of a single on/off cell
+    pre = singleCellPredecessorsStrict()
+
+    # populate init_cells
+    for row in range(height):
+        for col in range(width):
+
+            pass
