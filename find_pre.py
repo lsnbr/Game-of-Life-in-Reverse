e28e3dc950d4d3d14feb78b8cae0179ba8cec72c
lsnbr <luis@sovt.de>
2020-04-29 10:55:39 +0200

    lala

--- find_pre.py
+++ find_pre.py
@@ -6,8 +6,9 @@ from quad_2n import quad_2n
 
 
 
-goal = create_rnd((4,4))
+goal = create_rnd((8,8), 0.8)
 
 pred = quad_2n(goal)
 
-print_life(*pred[:min(len(pred), 4)])
+print_life(goal, title='Goal pattern')
+print_life(pred[0], title=f'{len(pred)} predecessors')
